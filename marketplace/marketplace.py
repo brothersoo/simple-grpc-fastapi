29624b7067c693c23817fda03a8e2e9ae8e7af07
brothersoo <moistybro@gmail.com>
2021-08-26 20:02:18 +0900

    ADD: docker, kubernetes settings, test code

--- marketplace/marketplace.py
+++ marketplace/marketplace.py
@@ -1,38 +1,47 @@
 import grpc
+import logging
 import os
 import uvicorn
 
 from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from google.protobuf.json_format import MessageToDict
 
-from recommendations.recommendations_pb2 import BookCategory, RecommendationRequest
-from recommendations.recommendations_pb2_grpc import RecommendationsStub
+from recommendations_pb2 import BookCategory, RecommendationRequest
+from recommendations_pb2_grpc import RecommendationsStub
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
 recommendations_host = os.getenv("RECOMMENDATIONS_HOST", "localhost")
+
 recommendations_channel = grpc.insecure_channel(
-    f"{recommendations_host}:50051"
+    f"{recommendations_host}:50051",
+    options=(('grpc.enable_http_proxy', 0),)
 )
 recommendations_client = RecommendationsStub(recommendations_channel)
 
-@app.get("/")
-def render_homepage():
+
+@app.get("/", response_class=HTMLResponse)
+async def render_homepage(request: Request):
     recommendations_request = RecommendationRequest(
         user_id=1, category=BookCategory.MYSTERY, max_results=3
     )
     recommendations_response = recommendations_client.Recommend(
         recommendations_request
     )
+    dict_response = MessageToDict(recommendations_response)
+    dict_response['request'] = request
     return templates.TemplateResponse(
         "homepage.html",
-        recommendations=recommendations_response.recommendations,
+        dict_response,
     )
 
 
 if __name__ == "__main__":
     uvicorn.run(app, host='0.0.0.0', port=8000)
+
